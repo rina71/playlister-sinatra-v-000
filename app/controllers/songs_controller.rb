566bcb31da64ca08b4492c742beb85daad27d877
Yurina Shin <yurinas@gmail.com>
2018-10-10 18:02:15 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,6 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.create(name:params[:Name])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-
     @song.genre_ids = params[:genres]
     @song.save
     flash[:message] = "Successfully created song."
