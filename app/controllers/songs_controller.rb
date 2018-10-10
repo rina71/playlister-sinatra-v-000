cdeaae90739d151b673224ec70634a54a3b04c30
Yurina Shin <yurinas@gmail.com>
2018-10-10 19:41:44 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,11 +38,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @song.name = params[:Name]
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-
     @song.genre_ids = params[:genres]
-
-
-
     @song.save
     flash[:message] = "Successfully updated song."
 
